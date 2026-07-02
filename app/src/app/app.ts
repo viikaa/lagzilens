3dc068eb4da918e74033ef2d51454d47f82039f8
Viktor Blanárik <viktor.blanarik@hemex.io>
2026-07-02 22:28:18 +0200

    enhance loading state

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  private router = inject(Router);
+  showLoader = signal(true);
+
+  constructor() {
+    this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationEnd ||
+            event instanceof NavigationCancel ||
+            event instanceof NavigationError
+        ),
+        take(1)
+      )
+      .subscribe(() => {
+        this.showLoader.set(false);
+      });
+  }
+}
